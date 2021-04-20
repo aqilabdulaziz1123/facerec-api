4d708becb3e67daaa214e5d31d76098e81831bb6
jafar-shodiq <72054858+jafar-shodiq@users.noreply.github.com>
2021-04-20 13:25:26 +0700

    updated dbSeed

--- dbSeed.py
+++ dbSeed.py
@@ -6,8 +6,7 @@ host = "127.0.0.1"
 port = 3306
 username = "root"
 password = ""
-# database = "facerec"
-database = "facerec_subsub"
+database = "facerec"
 
 try:
     con = connect(host=host, port=port, user=username, password=password, db=database)
